f1308cbc87a6a75ff81ce9977bce2ef8045777fb
Andri-Almengor <120224177+Andri-Almengor@users.noreply.github.com>
2026-05-27 08:21:33 -0600

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-@echo off
-set RENDER_DATABASE_URL=postgresql://USER:PASSWORD@HOST:5432/DATABASE
-pg_restore --clean --if-exists --no-owner --no-privileges -d "%RENDER_DATABASE_URL%" kosher_local.dump
-echo.
-echo Import terminado.
-pause
